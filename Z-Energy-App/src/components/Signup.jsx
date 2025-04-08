e0d8080370c2607a3f8c25e2293cf1633ac0ca11
Harika Vonguri <harikavonguri@gmail.com>
2025-04-08 13:14:02 +1200

    Login page code and API for the same added

--- Z-Energy-App/src/components/Signup.jsx
+++ Z-Energy-App/src/components/Signup.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 import styles from "./Signup.module.css";
+import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
@@ -14,7 +17,11 @@ function Signup() {
         email,
         password,
       })
-      .then((result) => console.log(result))
+      .then((result) => {
+        console.log(result);
+        navigate("/Login");
+        alert("Registered Successfully");
+      })
       .catch((error) => console.log(error));
   };
   return (
--- backend/index.js
+++ backend/index.js
@@ -16,3 +16,20 @@ app.post("/register", (req, res) => {
     .then((users) => res.json(users))
     .catch((err) => res.json(err));
 });
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  UserModel.findOne({ email, password })
+    .then((user) => {
+      if (user) {
+        if (user.password === password) {
+          res.status(200).json("sucessfully logged in");
+        } else {
+          res.status(401).json({ message: "Invalid credentials" });
+        }
+      } else {
+        res.status(404).json({ message: "User not found" });
+      }
+    })
+
+    .catch((err) => res.json(err));
+});
